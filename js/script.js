8c72ffb1b95d89dc36c3931675510635d46443fb
Mustafa Yildirim <macityildirimm@gmail.com>
2021-07-18 13:45:26 +0300

    project completed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
+const form = document.querySelector('.footer__newsletter__form');
+const emailInput = form.querySelector('.footer__newsletter__input');
+const tooltip = document.querySelector('.footer__tooltip');
+const tooltipTwo = document.querySelector('.footer__tooltip-two');
+
+const testimonialDots = document.querySelectorAll('.testimonials__dot');
+const testimonials = document.querySelectorAll('.testimonial');
+const testinmonialsArray = Array.from(testimonials);
+/*------------------------ Responsive Menu-------------------------*/
 function menuToggle() {
     const menuToggle = document.querySelector(".navbar__toggle");
     const list = document.querySelector(".navbar__list");
     if (menuToggle.classList.value.includes("active")) {
-      console.log("macit");
-
+      //console.log("test nav");
       list.classList.add("hide");
     } else {
       list.classList.remove("hide");
@@ -11,14 +19,8 @@ function menuToggle() {
     menuToggle.classList.toggle("active");
     list.classList.toggle("active");
   }
-  window.addEventListener("scroll", function () {
-    const navbar = document.querySelector(".navbar");
-    navbar.classList.toggle("sticky", window.scrollY > 0);
-  });
 
-  const testimonialDots = document.querySelectorAll('.testimonials__dot');
-  const testimonials = document.querySelectorAll('.testimonial');
-  const testinmonialsArray = Array.from(testimonials);
+/*----------------------Auto Slid---------------------------- */
   testimonialDots.forEach((testimonialDot) => {
   testimonialDot.addEventListener('click', () => {
   testimonialDots.forEach((testimonialDot) => {
@@ -36,3 +38,24 @@ function menuToggle() {
 });
 
 
+/*-----------------------Form Validaiton-------------------------------------------*/
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  const emailInputValue = e.target.email.value;
+  const regexEmail = /^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$/;
+  if (!regexEmail.test(emailInputValue)) {
+    tooltip.style.display = 'block';
+    emailInput.style.border = '2px solid hsl(0, 88%, 59%)';
+    emailInput.style.color = 'hsl(0, 88%, 59%)';
+  } else {
+    tooltip.style.display = 'none';
+    tooltipTwo.style.display = 'block';
+    emailInput.style.border = 'none';
+    emailInput.style.color = 'unset';
+    setTimeout(function () {
+      tooltipTwo.style.display = 'none';
+    }, 3000);
+    form.reset();
+  }
+});
